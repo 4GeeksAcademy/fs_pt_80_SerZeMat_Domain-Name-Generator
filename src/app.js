b2859d5927757556610346ad1356da1901eddb60
SerZeMat <secemacorp@gmail.com>
2024-10-09 20:17:29 +0000

    Generador actualizado de forma Randomizada sin Arrow Funct y con Responsabilidad Única

--- src/app.js
+++ src/app.js
@@ -8,22 +8,27 @@ import "./assets/img/4geeks.ico";
 //Creamos Script que genere todas las combinaciones posibles de nombres de dominio
 //de una lista de pronombres, adjetivos, sustantivos, y ademas empelando varios dominios
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"]; 
-let domain = [".com", ".es", ".net", ".us", "org", ".io"];
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es", ".net", ".us", "org", ".io"];
 
-
-for (let i in pronoun){
-  for (let j in adj) {
-    for (let k in noun) {
-      for (let l in domain) {
-        console.log(`${i}${j}${k}${l}`);
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let k in noun) {
+        for (let l in domain) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`);
+        }
       }
     }
   }
-}
 
-//Ulilizando el principio de Responsabilidad Única
+  //Ulilizando el principio de Responsabilidad Única
 
-pronoun.forEach(i => adj.forEach(j => noun.forEach(k => domain.forEach(l => console.log(`${i}${j}${k}${l}`)))));
+  pronoun.forEach(i =>
+    adj.forEach(j =>
+      noun.forEach(k => domain.forEach(l => console.log(`${i}${j}${k}${l}`)))
+    )
+  );
+};
