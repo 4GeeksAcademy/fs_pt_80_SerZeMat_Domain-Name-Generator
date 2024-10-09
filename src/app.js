0a284a7b791c06ed1d12baa0c8f30dffced6d953
SerZeMat <secemacorp@gmail.com>
2024-10-09 16:27:09 +0000

    Generador dominio  de forma Randomizada y con Responsabilidad Única

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Creamos Script que genere todas las combinaciones posibles de nombres de dominio
+//de una lista de pronombres, adjetivos, sustantivos, y ademas empelando varios dominios
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"]; 
+let domain = [".com", ".es", ".net", ".us", "org", ".io"];
+
+
+for (let i in pronoun){
+  for (let j in adj) {
+    for (let k in noun) {
+      for (let l in domain) {
+        console.log(`${i}${j}${k}${l}`);
+      }
+    }
+  }
+}
+
+//Ulilizando el principio de Responsabilidad Única
+
+pronoun.forEach(i => adj.forEach(j => noun.forEach(k => domain.forEach(l => console.log(`${i}${j}${k}${l}`)))));
